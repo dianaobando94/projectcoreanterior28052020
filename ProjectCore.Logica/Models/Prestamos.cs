c29f9597216d01d7b9ef38ae7f0d8e70d8b70068
Diana Obando <dianaobando94@hotmail.com>
2020-05-27 20:43:40 -0500

    Inicial commit

---
+++ ProjectCore.Logica/Models/Prestamos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectCore.Logica.Models
+{
+    class Prestamos
+    {
+    }
+}
---
+++ ProjectCore.Logica/Models/Projects.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectCore.Logica.Models
+{
+   public class Projects : DAL.Models.Projects
+    {
+
+    }
+}
